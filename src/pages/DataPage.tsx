302d52394f4ed8ae5e53233ac03b13c70e427fa7
Remco Goyvaerts <remco.goyvaerts@acagroup.be>
2025-01-23 10:24:56 +0100

    Fix css

--- src/pages/DataPage.tsx
+++ src/pages/DataPage.tsx
@@ -5,7 +5,7 @@ export default function DataPage() {
   const { data } = useDataStore();
 
   return (
-    <div className="flex h-full flex-col items-center justify-center gap-2">
+    <div className="flex h-full flex-col items-center gap-2">
       <div className="w-full border border-white">
         <div className="flex flex-col gap-2">
           {data.map((row, i) => (
